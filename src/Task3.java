037299fef231d84b6d6c85101155334d1b08d0b6
ChristianMcNair <mcnairce@mail.uc.edu>
2025-03-25 10:32:11 -0400

    Final commit

--- src/Task3.java
+++ src/Task3.java
@@ -1,16 +1,11 @@
 import java.util.Random;
-
 public class Task3 {
     public static void main(String[] args) {
 
         int[] dataPoints = {1, 2, 3, 4, 5};
-
-        // First loop to process dataPoints (if needed)
         for (int i = 0; i < dataPoints.length; i++) {
-            // Processing logic can go here
-        }
 
-        // Second loop to display dataPoints values
+        }
         for (int i = 0; i < dataPoints.length; i++) {
             System.out.print(dataPoints[i]);
             if (i < dataPoints.length - 1) {
